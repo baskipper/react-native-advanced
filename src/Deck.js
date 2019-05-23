40a7026307f329341933bd252ecee99f037a9b5a
bskipper <baskipper67@gmail.com>
2019-05-23 10:24:19 -0500

    Whole list is now draggable

--- src/Deck.js
+++ src/Deck.js
@@ -1,17 +1,37 @@
 import React, {Component} from 'react';
-import {View, Animated} from 'react-native';
+import {View, Animated, PanResponder} from 'react-native';
 
 class Deck extends Component {
+    constructor(props) {
+        super(props)
+
+        const position = new Animated.ValueXY()
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, {dx, dy}) => {
+                position.setValue({x: dx , y:dy })
+            },
+            onPanResponderRelease: () => {
+            }
+        });
+
+        this.state = {panResponder, position};
+    }
+
     renderCards() {
         return this.props.data.map(item =>
             this.props.renderCard(item)
         )
     }
+
     render() {
-        return(
-            <View >
+        return (
+            <Animated.View
+                style={this.state.position.getLayout()}
+                {...this.state.panResponder.panHandlers}
+            >
                 {this.renderCards()}
-            </View>
+            </Animated.View>
         )
     }
 }
