c0bab205a131f79fc13047eb7d15e155a150e039
bskipper <baskipper67@gmail.com>
2019-05-23 11:38:00 -0500

    Card position smoothly resets on release

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,12 @@
 import React, {Component} from 'react';
-import {View, Animated, PanResponder} from 'react-native';
+import {
+    View,
+    Animated,
+    PanResponder,
+    Dimensions
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
     constructor(props) {
@@ -9,29 +16,54 @@ class Deck extends Component {
         const panResponder = PanResponder.create({
             onStartShouldSetPanResponder: () => true,
             onPanResponderMove: (event, {dx, dy}) => {
-                position.setValue({x: dx , y:dy })
+                position.setValue({x: dx, y: dy})
             },
-            onPanResponderRelease: () => {
-            }
+            onPanResponderRelease: () => this.resetPosition()
+            ,
         });
 
         this.state = {panResponder, position};
     }
 
+    resetPosition() {
+        Animated.spring(this.state.position,  {
+            toValue: {x:0, y:0}
+        }).start()
+    }
+
+    getCardStyle() {
+        const {position} = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        })
+
+        return {
+            ...position.getLayout(),
+            transform: [{rotate}]
+        }
+    }
+
     renderCards() {
-        return this.props.data.map(item =>
-            this.props.renderCard(item)
+        return this.props.data.map((item, index) =>
+            index === 0 ? (
+                <Animated.View
+                    key={item.id}
+                    style={this.getCardStyle()}
+                    {...this.state.panResponder.panHandlers}
+                >
+                    {this.props.renderCard(item)}
+                </Animated.View>
+            ) : this.props.renderCard(item)
         )
     }
 
     render() {
         return (
-            <Animated.View
-                style={this.state.position.getLayout()}
-                {...this.state.panResponder.panHandlers}
+            <View
             >
                 {this.renderCards()}
-            </Animated.View>
+            </View>
         )
     }
 }
